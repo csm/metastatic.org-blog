63e2e5db76c9d6180798c00adaafdb12404f6b46
Casey Marshall <casey@metastatic.org>
2022-09-11 19:02:11 -0700

    Simplify.

--- src/cryogen/push.clj
+++ src/cryogen/push.clj
@@ -1,28 +1,19 @@
 (ns cryogen.push
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :as shell])
   (:import (java.io PushbackReader)
            (java.lang ProcessBuilder$Redirect)))
-
-(defn sh-
-  [& args]
-  (println "running process: " args)
-  (let [p (-> (ProcessBuilder. args)
-              (.directory (io/file "public"))
-              (.redirectOutput ProcessBuilder$Redirect/PIPE)
-              (.redirectError ProcessBuilder$Redirect/PIPE)
-              (.redirectInput ProcessBuilder$Redirect/PIPE)
-              (.start))]
-    (.waitFor p)
-    (when (not (zero? (.exitValue p)))
-      (throw (Exception. (str "process exited with status " (.exitValue p)))))))
 
 (defn -main [& args]
   (when-let [private-key (System/getenv "SSH_PRIVATE_KEY")]
     (.mkdir (io/file (str (System/getProperty "user.home") "/.ssh")))
     (spit (str (System/getProperty "user.home") "/.ssh/test")
           private-key))
-  (let [config (read (PushbackReader. (io/reader "content/config.edn")))]
-    (sh- "rsync"
-         "-av"
-         "."
-         (str (:ssh-user config) \@ (:ssh-host config) \: (:rsync-path config)))))
+  (let [config (read (PushbackReader. (io/reader "content/config.edn")))
+        result (shell/sh "rsync"
+                         "-av"
+                         "."
+                         (str (:ssh-user config) \@ (:ssh-host config) \: (:rsync-path config)))]
+    (when (not (zero? (:exit result)))
+      (prn "push failed"
+           result))))
